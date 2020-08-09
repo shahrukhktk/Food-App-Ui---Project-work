34c56fcb37403ec3e73d1bd00289eb4e3d7c4da5
shahrukhktk <Shahrukhktk760@gmail.com>
2020-08-09 00:41:11 -0700

    init login & sign up done Order Confirmation (pending) Shop List Done

--- lib/screens/listofshops.dart
+++ lib/screens/listofshops.dart
@@ -33,7 +33,75 @@ class _ShopListState extends State<ShopList> {
                 ],
               ),
             ),
-          )
+          ),
+
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: SizedBox(
+                      height: 40,
+                      width: 140,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 225, 225, 225),
+                          borderRadius: BorderRadius.circular(25)
+                          ),
+                        child: Center(
+                          child: Text('28 Business Near You', style: TextStyle(
+                            fontFamily: 'SFProDisplay', fontSize: 13.0, fontWeight: FontWeight.normal, color: Colors.black,
+                          ),),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: SizedBox(
+                      height: 40,
+                      width: 80,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(25)
+                        ),
+                        child: Center(
+                          child: Text('Delivery', style: TextStyle(
+                            fontFamily: 'SFProDisplay', fontSize: 13.0, fontWeight: FontWeight.normal, color: Colors.white,
+                          ),),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: SizedBox(
+                      height: 40,
+                      width: 120,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 225, 225, 225),
+                            borderRadius: BorderRadius.circular(25)
+                        ),
+                        child: Center(
+                          child: Text('Store Clearance', style: TextStyle(
+                            fontFamily: 'SFProDisplay', fontSize: 13.0, fontWeight: FontWeight.normal, color: Colors.black,
+                          ),),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          
 
         ],
       ),
